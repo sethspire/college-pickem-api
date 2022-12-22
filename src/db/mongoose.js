0ebf055e3485c84020a799bb8cccf94d15d8d17e
sethspire <sspire2@eagles.bridgewater.edu>
2022-12-21 23:51:57 -0500

    set strictQuery to true

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 console.log(`Connecting to ${process.env.MONGODB_URL}`)
+mongoose.set('strictQuery', true)
 mongoose.connect(process.env.MONGODB_URL).catch((e) => {
     console.log(e)
 })
